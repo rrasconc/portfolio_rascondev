424c8737608d27a720af6b603698acb81fb9f304
Rigoberto Rascon <rigobertorascon@Rigobertos-MacBook-Pro.local>
2022-02-01 10:38:55 -0700

    Update fade-in

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+import { Navbar } from "../../components/Navbar/Navbar";
+
+export const Home = () => {
+  return (
+    <div className="container">
+      <h2 className="header fade-in">RIGOBERTO RASCON</h2>
+      <h2 className="sub header fade-in">SOFTWARE DEVELOPER</h2>
+    </div>
+  );
+};
