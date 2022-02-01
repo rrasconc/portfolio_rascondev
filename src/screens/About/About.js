1aba8ea65f33a7f9d07e2b3a1e0ef5b838a04f81
Rigoberto Rascon <rigobertorascon@Rigobertos-MacBook-Pro.local>
2022-02-01 16:38:08 -0700

    Show tech stack

--- src/screens/About/About.js
+++ src/screens/About/About.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./About.css";
 
 const profile = require("../../assets/profile.jpeg");
 
 export const About = () => {
+  const [isStackVisible, setStackVisible] = useState(false);
+
   return (
-    <div className="container">
-      <img src={profile} className="profile fade-in" />
-      <div className="description fade-in">
-        I am a <span className="highlight">software engineer</span> currently
-        working as a mobile developer. I am really passionate about the frontend
-        development and the UI designing.
+    <div>
+      <div className="screen">
+        {!isStackVisible ? (
+          <div className="container fade-in">
+            <img src={profile} className="profile" />
+            <div className="description">
+              I am a <span className="highlight">software engineer </span>
+              currently working as a mobile developer. I am really passionate
+              about the frontend development and the UI designing.
+            </div>
+            <button
+              className="button"
+              onClick={() => {
+                setStackVisible("stack");
+              }}
+            >
+              Tech stack
+            </button>
+          </div>
+        ) : (
+          <div className="container fade-in">
+            <div className="description">This is my tech stack</div>
+          </div>
+        )}
       </div>
     </div>
   );
