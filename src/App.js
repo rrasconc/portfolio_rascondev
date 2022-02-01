3bec81fcc0d27f39799135d058c3539694e24322
Rigoberto Rascon <rigobertorascon@Rigobertos-MacBook-Pro.local>
2022-02-01 10:00:37 -0700

    Control navbar globaly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from "./logo.svg";
+import react, { useState } from "react";
 import "./App.css";
+import "./assets/styles.css";
 import { Home } from "./screens/Home";
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState("about");
+
   return (
     <div className="screen">
       <Home />
-      <Navbar />
+      <Navbar
+        selectedItem={selectedItem}
+        handlePress={(item) => setSelectedItem(item)}
+      />
     </div>
   );
 }
