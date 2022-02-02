533bc7a3533ed07270285c893e0e73fe77d6374f
Rigoberto Rascon <rigobertorascon@Rigobertos-MacBook-Pro.local>
2022-02-02 13:15:07 -0700

    Fix containers

--- src/screens/About/About.js
+++ src/screens/About/About.js
@@ -12,52 +12,62 @@ const stack = [
     name: "CSS",
     icon: require("../../assets/css.png"),
   },
+  {
+    name: "SQL",
+    icon: require("../../assets/sql.png"),
+  },
+  {
+    name: "Figma",
+    icon: require("../../assets/figma.png"),
+  },
 ];
 
 export const About = () => {
   const [isStackVisible, setStackVisible] = useState(false);
   const [stackTitle, setStackTitle] = useState("This is my tech stack");
+  const [stackDescription, setStackDescription] = useState(
+    "Hover an icon to see more"
+  );
 
   return (
-    <div>
-      <div className="screen">
-        {!isStackVisible ? (
-          <div className="container">
-            <img src={profile} className="profile" />
-            <div className="description  fade-in">
-              I'm a <span className="highlight">software engineer </span>
-              currently working as a mobile developer. I am really passionate
-              about the frontend development and the UI designing.
-            </div>
-            <button
-              className="button  fade-in"
-              onClick={() => {
-                setStackVisible("stack");
-              }}
-            >
-              Tech stack
-            </button>
+    <div className="container">
+      {!isStackVisible ? (
+        <div>
+          <img src={profile} className="profile" />
+          <div className="description  fade-in">
+            I'm a <span className="highlight">software engineer </span>
+            currently working as a mobile developer. I am really passionate
+            about the frontend development and the UI designing.
           </div>
-        ) : (
-          <div className="stack fade-in">
-            <h2 className="title">{stackTitle}</h2>
-            <div className="stack-icons">
-              {stack.map((item) => {
-                return (
-                  <img
-                    key={item.name}
-                    src={item.icon}
-                    className="icon m-10"
-                    onMouseEnter={() => {
-                      setStackTitle(item.name);
-                    }}
-                  />
-                );
-              })}
-            </div>
+          <button
+            className="button  fade-in"
+            onClick={() => {
+              setStackVisible("stack");
+            }}
+          >
+            Tech stack
+          </button>
+        </div>
+      ) : (
+        <div className="stack fade-in">
+          <h2 className="title">{stackTitle}</h2>
+          <div className="stack-icons">
+            {stack.map((item) => {
+              return (
+                <img
+                  key={item.name}
+                  src={item.icon}
+                  className="icon m-10"
+                  onMouseEnter={() => {
+                    setStackTitle(item.name);
+                  }}
+                />
+              );
+            })}
           </div>
-        )}
-      </div>
+          <div className="description">{stackDescription}</div>
+        </div>
+      )}
     </div>
   );
 };
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Home.css";
-import { Navbar } from "../../components/Navbar/Navbar";
 
 export const Home = () => {
   return (
